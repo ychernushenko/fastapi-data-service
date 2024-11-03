07063bd03a75c5237197b0429f1881e09c9090f9
Yuri Chernushenko <y.chernushenko@gmail.com>
2024-11-03 20:30:47 +0100

    fix diagram

--- src/utils/gcp_diagram.py
+++ src/utils/gcp_diagram.py
@@ -3,7 +3,8 @@ from diagrams.gcp.compute import Run
 from diagrams.gcp.database import SQL
 from diagrams.gcp.compute import Functions
 from diagrams.gcp.analytics import Pubsub
+from diagrams.gcp.security import IAP
 
 with Diagram("GCP Diagram", show=False):
-    Run("Container with FastAPI") >> Pubsub("Message Queue") >> Functions(
-        "Python Consumer Function") >> SQL("Postgres")
+    IAP("Authentication") >> Run("Container with FastAPI") >> Pubsub("Message Queue") >> Functions(
+        "Python Processing Function") >> SQL("Postgres")
